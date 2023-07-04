4b365cd9eea926d9d23eaef7033945d2fd06419d
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-07-04 17:00:43 -0400

    project created

---
+++ src/main/java/com/mahiiru/prograavanzadaspringboot/PrograAvanzadaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mahiiru.prograavanzadaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrograAvanzadaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrograAvanzadaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahiiru/prograavanzadaspringboot/PrograAvanzadaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahiiru.prograavanzadaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrograAvanzadaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
